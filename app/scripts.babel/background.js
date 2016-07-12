5b247e91003b4e178380cac4d37a9ee55566b5fe
Dario Alvarez <dario17231@gmail.com>
2016-07-12 23:06:43 +0100

    First commit

---
+++ app/scripts.babel/background.js
@@ -0,0 +1,12 @@
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  console.log('previousVersion', details.previousVersion);
+});
+console.log("SI");
+chrome.browserAction.onClicked.addListener(function (tab) {
+	// for the current tab, inject the "inject.js" file & execute it
+	chrome.tabs.executeScript(tab.ib, {
+		file: 'scripts/spModalLauncher.js'
+	});
+});
---
+++ app/scripts.babel/spModalLauncher.js
@@ -0,0 +1,5 @@
+var scr = document.createElement('script');
+scr.src="https://rawgit.com/DariuS231/SpPropertyBag.js/master/SpPropertyBag.js";
+(document.head || document.documentElement).appendChild(scr);
+//and then hide the evidence as much as possible.
+scr.parentNode.removeChild(scr);
