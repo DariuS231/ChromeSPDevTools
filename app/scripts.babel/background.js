f2f0f2187f3f8a98149c012d3cc4192f16aa227b
Dario Alvarez <dario17231@gmail.com>
2016-07-12 23:22:11 +0100

    Changed some strings from double quotes to single

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -3,7 +3,7 @@
 chrome.runtime.onInstalled.addListener(function (details) {
   console.log('previousVersion', details.previousVersion);
 });
-console.log("SI");
+
 chrome.browserAction.onClicked.addListener(function (tab) {
 	// for the current tab, inject the "inject.js" file & execute it
 	chrome.tabs.executeScript(tab.ib, {
