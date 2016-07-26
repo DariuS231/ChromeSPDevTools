b3023549cac8d1cbde6c13ef692c8a18164cef29
DariuS231 <dario17231@gmail.com>
2016-07-26 14:43:36 +0100

    Changed the url of the cdn

--- src/scripts/spModalLauncher.ts
+++ src/scripts/spModalLauncher.ts
@@ -1,7 +1,7 @@
 'use strict';
 
 var scr = document.createElement('script');
-scr.src = 'https://rawgit.com/DariuS231/SpPropertyBag.js/master/dist/SpPropertyBag.js';
+scr.src = 'https://cdn.rawgit.com/DariuS231/ChromeSPPropertiesAdmin/master/spPropertyBag/SpPropertyBag.js';
 (document.head || document.documentElement).appendChild(scr);
 //and then hide the evidence as much as possible.
 scr.parentNode.removeChild(scr);
