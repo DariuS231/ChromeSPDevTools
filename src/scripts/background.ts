6f891b9ba8d14db4b7d97103564a4efcc76ecc64
DariuS231 <dario17231@gmail.com>
2016-07-27 11:30:53 +0100

    Updated the url of the cdn

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,6 +1,4 @@
-
 /// <reference path="../../typings/chrome/chrome.d.ts" />
-'use strict';
 chrome.browserAction.onClicked.addListener(function (tab) {
 	chrome.tabs.executeScript(tab.id, {
 		file: 'scripts/spModalLauncher.js'
@@ -15,15 +13,13 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 			code: 'document.querySelector(\'meta[name="GENERATOR"]\').content'
 		}, function (result){
 			if(result.length > 0 && result[0] === "Microsoft SharePoint"){
-				console.log("Tab " + tabId + " is SharePoint!");
 				chrome.browserAction.enable(tabId);
-				chrome.browserAction.setBadgeText({text:'',tabId:tabId});
-				chrome.browserAction.setTitle({title:'Click to show the Properties modal.',tabId:tabId});
+				chrome.browserAction.setBadgeText({text:'',tabId});
+				chrome.browserAction.setTitle({title:'Click to show the Properties modal.',tabId});
 			}else{
-				console.log("Tab " + tabId + " is not SharePoint!");
 				chrome.browserAction.disable(tabId);
-				chrome.browserAction.setBadgeText({text:'X',tabId:tabId});
-				chrome.browserAction.setTitle({title:'DISABLED - Not a SharePoint site.',tabId:tabId});
+				chrome.browserAction.setBadgeText({text:'X',tabId});
+				chrome.browserAction.setTitle({title:'DISABLED - Not a SharePoint site.',tabId});
 			}
 		});
   }
--- src/scripts/spModalLauncher.ts
+++ src/scripts/spModalLauncher.ts
@@ -1,7 +1,9 @@
-'use strict';
+// For each new version of the script file, a new URL must be generated 
+// from the https://rawgit.com/ site using the url of the specific commit 
+let cdnUrl:string = 'https://cdn.rawgit.com/DariuS231/ChromeSPPropertiesAdmin/e6466f8867ccb464ae57d1b7f746d3b63507aced/src/scripts/SpPropertyBag/SpPropertyBag.ts';
 
-var scr = document.createElement('script');
-scr.src = 'https://cdn.rawgit.com/DariuS231/ChromeSPPropertiesAdmin/master/spPropertyBag/SpPropertyBag.js';
-(document.head || document.documentElement).appendChild(scr);
+let script:HTMLScriptElement = document.createElement('script');
+script.src = cdnUrl;
+(document.head || document.documentElement).appendChild(script);
 //and then hide the evidence as much as possible.
-scr.parentNode.removeChild(scr);
+script.parentNode.removeChild(script);
