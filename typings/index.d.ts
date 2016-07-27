c9ae53820a61c2a0b6131f90002da63dbf651b98
DariuS231 <dario17231@gmail.com>
2016-07-27 12:45:25 +0100

    Removed typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,6 +1,1 @@
-/// <reference path="globals/filesystem/index.d.ts" />
-/// <reference path="globals/filewriter/index.d.ts" />
-/// <reference path="globals/webrtc/mediastream/index.d.ts" />
-/// <reference path="globals/microsoft.ajax/index.d.ts" />
 /// <reference path="globals/sharepoint/index.d.ts" />
-/// <reference path="globals/chrome/index.d.ts" />
