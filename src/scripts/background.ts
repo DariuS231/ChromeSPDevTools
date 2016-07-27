9b20bd903cc235c8b6034d2775381310f0de543a
DariuS231 <dario17231@gmail.com>
2016-07-27 17:40:25 +0100

    Extension disabled on load

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,11 +1,15 @@
 /// <reference path="../../typings/globals/chrome/index.d.ts" />
+
+chrome.browserAction.disable();
+chrome.browserAction.setBadgeBackgroundColor({color:'red'});
+chrome.browserAction.setBadgeText({text:'X'});
+chrome.browserAction.setTitle({title:'DISABLED - Not a SharePoint site.'});
+
 chrome.browserAction.onClicked.addListener(function (tab) {
 	chrome.tabs.executeScript(tab.id, {
 		file: 'scripts/spModalLauncher.js'
 	});
 });
-
-chrome.browserAction.setBadgeBackgroundColor({color:'red'});
 
 chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
   	if (changeInfo.status == 'complete') {
@@ -16,10 +20,6 @@ chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
 				chrome.browserAction.enable(tabId);
 				chrome.browserAction.setBadgeText({text:'',tabId});
 				chrome.browserAction.setTitle({title:'Click to show the Properties modal.',tabId});
-			}else{
-				chrome.browserAction.disable(tabId);
-				chrome.browserAction.setBadgeText({text:'X',tabId});
-				chrome.browserAction.setTitle({title:'DISABLED - Not a SharePoint site.',tabId});
 			}
 		});
   }
