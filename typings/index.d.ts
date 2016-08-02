1e0feac764d85ee357580e8f0cba99c0d4aa11e3
Dario Alvarez <dario17231@gmail.com>
2016-08-02 19:35:36 +0100

    New index typings file.

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
+/// <reference path="custom/custom.d.ts" />
 /// <reference path="globals/chrome/index.d.ts" />
 /// <reference path="globals/es6-shim/index.d.ts" />
 /// <reference path="globals/filesystem/index.d.ts" />
@@ -5,5 +6,3 @@
 /// <reference path="globals/microsoft.ajax/index.d.ts" />
 /// <reference path="globals/sharepoint/index.d.ts" />
 /// <reference path="globals/webrtc/mediastream/index.d.ts" />
-
-/// <reference path="custom/custom.d.ts" />
