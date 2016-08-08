fbfcaaa2b23421f7f137d4999e54b4dd2f56b143
Fran Rodriguez <f.x.rodriguez@hotmail.com>
2016-08-08 17:53:46 +0100

    <meta name="generator"> null check

--- src/scripts/background.ts
+++ src/scripts/background.ts
@@ -1,9 +1,9 @@
 /// <reference path="../../typings/globals/chrome/index.d.ts" />
 
 chrome.browserAction.disable();
-chrome.browserAction.setBadgeBackgroundColor({color:'red'});
-chrome.browserAction.setBadgeText({text:'X'});
-chrome.browserAction.setTitle({title:'DISABLED - Not a SharePoint site.'});
+chrome.browserAction.setBadgeBackgroundColor({ color: 'red' });
+chrome.browserAction.setBadgeText({ text: 'X' });
+chrome.browserAction.setTitle({ title: 'DISABLED - Not a SharePoint site.' });
 
 chrome.browserAction.onClicked.addListener(function (tab) {
 	chrome.tabs.executeScript(tab.id, {
@@ -11,16 +11,19 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 	});
 });
 
-chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
-  	if (changeInfo.status == 'complete') {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+	if (changeInfo.status == 'complete') {
 		chrome.tabs.executeScript(tabId, {
-			code: 'document.querySelector(\'meta[name="GENERATOR"]\').content'
-		}, function (result){
-			if(result.length > 0 && result[0] === "Microsoft SharePoint"){
+			code: `(function() { 
+				var meta = document.querySelector(\'meta[name="GENERATOR"]\');
+				return meta && meta.content
+			})()`
+		}, function (result) {
+			if (result.length > 0 && result[0] === "Microsoft SharePoint") {
 				chrome.browserAction.enable(tabId);
-				chrome.browserAction.setBadgeText({text:'',tabId});
-				chrome.browserAction.setTitle({title:'Click to show the Properties modal.',tabId});
+				chrome.browserAction.setBadgeText({ text: '', tabId });
+				chrome.browserAction.setTitle({ title: 'Click to show the Properties modal.', tabId });
 			}
 		});
-  }
+	}
 });
