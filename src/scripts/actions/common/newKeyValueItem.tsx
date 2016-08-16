d6c6d56965d2ae0e6bb80add7f11632071a51879
DariuS231 <dario17231@gmail.com>
2016-08-16 14:26:36 +0100

    Added design to the new item module

--- src/scripts/actions/common/newKeyValueItem.tsx
+++ src/scripts/actions/common/newKeyValueItem.tsx
@@ -36,14 +36,34 @@ export default class NewKeyValueItem extends React.Component<NewKeyValueItemProp
        this.setState({ newValue: e.target.value } as NewKeyValueItemState);
     }
     public render() {
-        /*
-        data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABPElEQVQ4T53TTyilYRTH8c+NspBskaYRykKGnaXNqGFmqWwlKRaTWZqlrCwmSihZSRYSym7KSlmNmt1kI1cWs5YsJJ163np7771yPfUs3nPe8z2/5/wpqTytmME87vAfA/iHTZzkQ0qF+HGsYxpneM75WzCVfF8SXB4wic+YxVMVZZnpY1LTFZAM0IsNjBayRtACfhWAATnCYAY4xHeUq2TeR6grnh+4CkC8bQ/fasiuBYhi7wZgBJ+wWicgfi8HYAINiExxDgqgYVzkbOe5mpwGYAwd2H6Hgj8B6MQi5uoEhOq/WReu0YfHOrrwFd0ZID6GsPRGQFOaxA/5SdzBcbqvDKLGaF+q2e88IN60lYZpBQ9VKD1pZpazRMVliphYlLW0TJe4R3uy3+InbjJ4NUDma0M/mtNKB6yiyC9HfD7GeS3R8QAAAABJRU5ErkJggg==
-        */
+        let btnStylePlus = {
+            display: 'inline-block',
+            height: '16px',
+            borderRadius: '5px',
+            padding: '3px 10px 4px 25px',
+            margin: '5px',
+            backgroundPosition: '10% 50%',
+            backgroundRepeat: 'no-repeat',
+            textDecoration: 'none',
+            color:'black',
+            fontWeight: 'bold',
+            backgroundColor: 'rgba(30,144,255,0.5)',
+            backgroundImage: 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABPElEQVQ4T53TTyilYRTH8c+NspBskaYRykKGnaXNqGFmqWwlKRaTWZqlrCwmSihZSRYSym7KSlmNmt1kI1cWs5YsJJ163np7771yPfUs3nPe8z2/5/wpqTytmME87vAfA/iHTZzkQ0qF+HGsYxpneM75WzCVfF8SXB4wic+YxVMVZZnpY1LTFZAM0IsNjBayRtACfhWAATnCYAY4xHeUq2TeR6grnh+4CkC8bQ/fasiuBYhi7wZgBJ+wWicgfi8HYAINiExxDgqgYVzkbOe5mpwGYAwd2H6Hgj8B6MQi5uoEhOq/WReu0YfHOrrwFd0ZID6GsPRGQFOaxA/5SdzBcbqvDKLGaF+q2e88IN60lYZpBQ9VKD1pZpazRMVliphYlLW0TJe4R3uy3+InbjJ4NUDma0M/mtNKB6yiyC9HfD7GeS3R8QAAAABJRU5ErkJggg==)'
+        };
+        let divStyle= { display: 'inline-block', width: '44%' };
+        let labelStyle = { display: 'block'};
+        let inputStyle={ width:'95%' };
         return <div>
             <h2>{this.props.moduleTitle}</h2>
-            <label htmlFor="newKey">{this.props.keyDisplayName}: </label><input id="newKey" value={this.state.newKey} onChange={this.onKeyInputChange.bind(this)} />
-            <label htmlFor="newValue">{this.props.valueDisplayName}: </label><input id="newValue" value={this.state.newValue} onChange={this.onValueInputChange.bind(this)} />
-            <a href="javascript:void(0)" onClick={this.addBtnClick.bind(this) }>Add</a>
+            <div style={divStyle}>
+                <label style={labelStyle} htmlFor="newKey">{this.props.keyDisplayName}: </label>
+                <input style={inputStyle} id="newKey" value={this.state.newKey} onChange={this.onKeyInputChange.bind(this)} />
+            </div>
+            <div style={divStyle}>
+                <label style={labelStyle} htmlFor="newValue">{this.props.valueDisplayName}: </label>
+                <input style={inputStyle} id="newValue" value={this.state.newValue} onChange={this.onValueInputChange.bind(this)} />
+            </div>
+            <a href="javascript:void(0)" style={btnStylePlus} onClick={this.addBtnClick.bind(this) }>Add</a>
         </div>;
     }
 }
