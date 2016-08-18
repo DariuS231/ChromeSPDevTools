6c1f5b20d62102deec238b9b5f87cd4079f798b9
DariuS231 <dario17231@gmail.com>
2016-08-18 17:31:41 +0100

    Removes styles from common components files

--- src/scripts/actions/common/newKeyValueItem.tsx
+++ src/scripts/actions/common/newKeyValueItem.tsx
@@ -1,7 +1,8 @@
 /// <reference path="../../../../typings/index.d.ts"/>
 /// <reference path="./interfaces.ts"/>
 import * as React from 'react';
-//import { NewKeyValueItemStyles as styles} from './styles'
+import { NewKeyValueItemStyles as nkvStyles } from './Styles'
+import Utils from './utils';
 
 interface NewKeyValueItemState {
     newKey: string,
@@ -37,45 +38,17 @@ export default class NewKeyValueItem extends React.Component<NewKeyValueItemProp
         this.setState({ newValue: e.target.value } as NewKeyValueItemState);
     }
     public render() {
-        let btnStyles = {
-            display: 'inline-block',
-            height: '16px',
-            borderRadius: '5px',
-            padding: '3px 10px 4px 25px',
-            margin: '5px',
-            backgroundPosition: '10% 50%',
-            backgroundRepeat: 'no-repeat',
-            textDecoration: 'none',
-            color: 'black',
-            fontWeight: 'bold'
-        }
-        let styles = {
-            btnStylePlus: Object.assign({
-                backgroundColor: 'rgba(30,144,255,0.5)',
-                backgroundImage: 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABPElEQVQ4T53TTyilYRTH8c+NspBskaYRykKGnaXNqGFmqWwlKRaTWZqlrCwmSihZSRYSym7KSlmNmt1kI1cWs5YsJJ163np7771yPfUs3nPe8z2/5/wpqTytmME87vAfA/iHTZzkQ0qF+HGsYxpneM75WzCVfF8SXB4wic+YxVMVZZnpY1LTFZAM0IsNjBayRtACfhWAATnCYAY4xHeUq2TeR6grnh+4CkC8bQ/fasiuBYhi7wZgBJ+wWicgfi8HYAINiExxDgqgYVzkbOe5mpwGYAwd2H6Hgj8B6MQi5uoEhOq/WReu0YfHOrrwFd0ZID6GsPRGQFOaxA/5SdzBcbqvDKLGaF+q2e88IN60lYZpBQ9VKD1pZpazRMVliphYlLW0TJe4R3uy3+InbjJ4NUDma0M/mtNKB6yiyC9HfD7GeS3R8QAAAABJRU5ErkJggg==)'
-            }, btnStyles),
-            divStyle: {
-                display: 'inline-block',
-                width: '44%'
-            },
-            labelStyle: {
-                display: 'block'
-            },
-            inputStyle: {
-                width: '95%'
-            }
-        }
         return <div>
             <h2>{this.props.moduleTitle}</h2>
-            <div style={styles.divStyle}>
-                <label style={styles.labelStyle} htmlFor="newKey">{this.props.keyDisplayName}: </label>
-                <input style={styles.inputStyle} id="newKey" value={this.state.newKey} onChange={this.onKeyInputChange.bind(this) } />
+            <div style={nkvStyles.divStyle}>
+                <label style={nkvStyles.labelStyle} htmlFor="newKey">{this.props.keyDisplayName}: </label>
+                <input style={nkvStyles.inputStyle} id="newKey" value={this.state.newKey} onChange={this.onKeyInputChange.bind(this) } />
             </div>
-            <div style={styles.divStyle}>
-                <label style={styles.labelStyle} htmlFor="newValue">{this.props.valueDisplayName}: </label>
-                <input style={styles.inputStyle} id="newValue" value={this.state.newValue} onChange={this.onValueInputChange.bind(this) } />
+            <div style={nkvStyles.divStyle}>
+                <label style={nkvStyles.labelStyle} htmlFor="newValue">{this.props.valueDisplayName}: </label>
+                <input style={nkvStyles.inputStyle} id="newValue" value={this.state.newValue} onChange={this.onValueInputChange.bind(this) } />
             </div>
-            <a href="javascript:void(0)" style={styles.btnStylePlus} onClick={this.addBtnClick.bind(this) }>Add</a>
+            <a href="javascript:void(0)" style={nkvStyles.btnStylePlus} onClick={this.addBtnClick.bind(this) }>Add</a>
         </div>;
     }
 }
--- src/scripts/actions/common/styles.ts
+++ src/scripts/actions/common/styles.ts
@@ -27,6 +27,44 @@ let inputStyles = {
     width: '240px'
 }
 
+export let KeyValueItemStyles = {
+    deleteBtnStyle: Utils.mergeObjects({
+        backgroundColor: 'rgba(255,0,0,0.2)',
+        backgroundImage: 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABa0lEQVQ4T33Tv0vWURTH8ZcYBIm0aogmJThE5eDekqDmIgRuIqKCDYGjbf2AoKlCVBAnCSdRwU3wPyhoixbJamiOaLCIA+fG5eujD3yH555z3vd8PvecNmd/VzGHR/iOH7iNT1jDfl3S1qgfxwpmcYS/VbwTMxkbTbgaMIX7WMBpi87K0fXspj8gBTCAVYw0bj2PE5Bd3C2AHTzGSVZMYwt/KkIf4qLDPFvC5wCEtneYqJInEX7MJySK11P/t8wLs7cCcA938LrRb4E8T3lhbCkuqScBeIh2bLcQHE/5NLRW8uq0gwCM4Ro2GoDSdpg1XMmp094HoAfLWGwYVmtuehKp0fXH8grHGMTvhLzFy4bmgFzJ14m0B7hRAPFnCM8uGKA6dDknsbeexE3s5XcR51J2EZ4d1oDQFLpjmF7hVwvKzZyZF+Wi5jJFTSzKm1ymD/iJ7jz/iif4UuCtACXWhVvoyJUOWDH5f3P/ALwNScZauIwdAAAAAElFTkSuQmCC)'
+    }, btnStyles),
+    updateBtnStyle: Utils.mergeObjects({
+        backgroundColor: 'rgba(255,255,0,0.5)',
+        backgroundImage: 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAA90lEQVQ4T6XTO0oEQRSF4W8CE8HERYjG4gM0MTSbQBegkTsw8hGYCQMG4mMBs4YZYzUUMVbMFMFExEciSEldaJqemW6spJL+/zp163TLP1erIT+HbWzhNbFNBPM4xwYuMJMkdQUJvsQKrjCV9+k6gjh5NSfYwS2O0B8lCHgJH5hAF0+4x+EwwQLOEHDM+wBvCR42xEFwiv8d8CDBIk4rTt7FVxGuEiT4BMv5zhF7D59luCxoDBcF43hGeqrrQjv3c5K/gVWteIVU0TWso407jISLCTbxktt1gx4eq+5cThEJOpjMRRlLDcNxnR8tBLN4xwN+6oDxzS/p9TduQiAy4gAAAABJRU5ErkJggg==)'
+    }, btnStyles),
+    saveBtnStyle: Utils.mergeObjects({
+        backgroundColor: 'rgba(50,205,50,0.5)',
+        backgroundImage: 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABPElEQVQ4T53TTyilYRTH8c+NspBskaYRykKGnaXNqGFmqWwlKRaTWZqlrCwmSihZSRYSym7KSlmNmt1kI1cWs5YsJJ163np7771yPfUs3nPe8z2/5/wpqTytmME87vAfA/iHTZzkQ0qF+HGsYxpneM75WzCVfF8SXB4wic+YxVMVZZnpY1LTFZAM0IsNjBayRtACfhWAATnCYAY4xHeUq2TeR6grnh+4CkC8bQ/fasiuBYhi7wZgBJ+wWicgfi8HYAIurl(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAn0lEQVQ4T92RzQnDMAyFP69RSsgW/VkgAySBTpFJskcptNeskHaPTJAJwqM2mGBjXXqpLhLi+ZP07PjGGah9bUkTsErovPoJtJaXXvMGGkFygB4QNITqzvfCoFkQK0Anaup+094KiDeJT80CPsCS8OQCHKJ+FmD18zeAR2F8BegURXKDYGyOcwPufw54FUw8Aqe9BwNwtX5+pBtLjheZG7dCJu50EjXIAAAAAElFTkSuQmCC)'
+    }, btnStyles),
+    oddTableRowStyles: { backgroundColor: 'rgba(100,149,237, 0.1)' },
+    headerStyle: Utils.mergeObjects({ textAlign: 'right', wordBreak: 'break-word' }, tableCellStyle),
+    tableCellStyle: tableCellStyle,
+    inputStyles: inputStyles,
+    inputReadOnlyStyles: Utils.mergeObjects({ backgroundColor: 'gainsboro' }, inputStyles),
+
+}
+
+export let NewKeyValueItemStyles = {
+    btnStylePlus: Utils.mergeObjects({
+        backgroundColor: 'rgba(30,144,255,0.5)',
+        backgroundImage: 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABPElEQVQ4T53TTyilYRTH8c+NspBskaYRykKGnaXNqGFmqWwlKRaTWZqlrCwmSihZSRYSym7KSlmNmt1kI1cWs5YsJJ163np7771yPfUs3nPe8z2/5/wpqTytmME87vAfA/iHTZzkQ0qF+HGsYxpneM75WzCVfF8SXB4wic+YxVMVZZnpY1LTFZAM0IsNjBayRtACfhWAATnCYAY4xHeUq2TeR6grnh+4CkC8bQ/fasiuBYhi7wZgBJ+wWicgfi8HYAINiExxDgqgYVzkbOe5mpwGYAwd2H6Hgj8B6MQi5uoEhOq/WReu0YfHOrrwFd0ZID6GsPRGQFOaxA/5SdzBcbqvDKLGaF+q2e88IN60lYZpBQ9VKD1pZpazRMVliphYlLW0TJe4R3uy3+InbjJ4NUDma0M/mtNKB6yiyC9HfD7GeS3R8QAAAABJRU5ErkJggg==)'
+    }, btnStyles),
+    divStyle: {
+        display: 'inline-block',
+        width: '44%'
+    },
+    labelStyle: {
+        display: 'block'
+    },
+    inputStyle: {
+        width: '95%'
+    }
+}
+
 export let MessageBarStyles = {
     span: {
         margin: 0,
