aca297ed6c48299966ec4eedc340912a4d19c507
DariuS231 <dario17231@gmail.com>
2016-08-24 16:55:44 +0100

    Site Content action

--- src/scripts/actions/common/interfaces.ts
+++ src/scripts/actions/common/interfaces.ts
@@ -2,3 +2,15 @@ interface IKeyValue {
     key: string,
     value: any
 }
+interface ISiteContent {
+    id: any,
+    title: any,
+    description:any,
+    hidden: any,
+    itemCount: any,
+    imageUrl: any,
+    created: any,
+    lastModified: any,
+    listUrl:any,
+    settingsUrl: string
+}
