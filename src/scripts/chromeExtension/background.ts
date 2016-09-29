49e9e4b497a555f00cb9c1fabf01d48bc9852459
Dario Alvarez <dario17231@gmail.com>
2016-09-29 23:13:45 +0100

    Added support for  modern pages

--- src/scripts/chromeExtension/background.ts
+++ src/scripts/chromeExtension/background.ts
@@ -1,5 +1,10 @@
 /// <reference path="../../../typings/index.d.ts" />
 
+var enableExt = function (tabId:number) {
+	chrome.browserAction.enable(tabId);
+	chrome.browserAction.setBadgeText({ text: '', tabId });
+	chrome.browserAction.setTitle({ title: 'Click to view the options.', tabId });
+}
 chrome.browserAction.disable();
 chrome.browserAction.setBadgeBackgroundColor({ color: 'red' });
 chrome.browserAction.setBadgeText({ text: 'X' });
@@ -11,19 +16,23 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 	});
 });
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+chrome.tabs.onUpdated.addListener(function (tabId:number, changeInfo:any, tab:any) {
 	if (changeInfo.status == 'complete') {
-		chrome.tabs.executeScript(tabId, {
-			code: `(function() { 
+		if (tab.url.indexOf('.sharepoint.com') >= 0) {
+			enableExt(tabId);
+		} else {
+			chrome.tabs.executeScript(tabId, {
+				code: `(function() { 
 				var meta = document.querySelector(\'meta[name="GENERATOR"]\');
 				return meta && meta.content
 			})()`
-		}, function (result) {
-			if (result.length > 0 && result[0] === "Microsoft SharePoint") {
-				chrome.browserAction.enable(tabId);
-				chrome.browserAction.setBadgeText({ text: '', tabId });
-				chrome.browserAction.setTitle({ title: 'Click to show the Properties modal.', tabId });
-			}
-		});
+			}, function (result) {
+				if (result.length > 0 && !!result[0] && result[0] === "Microsoft SharePoint") {
+					enableExt(tabId);
+				}
+			});
+		}
+
+
 	}
 });
