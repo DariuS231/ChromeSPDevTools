1c411ab1b72e82790d71ac3ae750f427f1808d35
Dario Alvarez <dario17231@gmail.com>
2016-12-15 01:05:05 +0000

    Refactoring

---
+++ src/scripts/actions/api/apiBase.ts
@@ -0,0 +1,37 @@
+
+
+declare var reject: any;
+
+export default class ApiBase {
+    ctx: SP.ClientContext;
+    web: SP.Web;
+    site: SP.Site;
+    constructor() {
+        this.ctx = SP.ClientContext.get_current();
+        this.web = this.ctx.get_web();
+        this.site = this.ctx.get_site();
+    }
+
+    public requestErrorEventHandler(sender: any, err: SP.ClientRequestFailedEventArgs): void {
+        reject(err.get_message());
+    }
+
+    public checkUserPermissions(permKind: SP.PermissionKind): Promise<boolean> {
+        return new Promise((resolve, reject) => {
+            if (typeof this.web.doesUserHavePermissions !== "function") {
+                reject('Cannot check permissions against a non-securable object.')
+            } else {
+                const ob: SP.BasePermissions = new SP.BasePermissions();
+                ob.set(permKind);
+                const per: any = this.web.doesUserHavePermissions(ob);
+
+                const onSuccess = (sender: any, args: SP.ClientRequestSucceededEventArgs) => {
+                    var hasPermissions = per.get_value();
+                    resolve(hasPermissions);
+                };
+                this.ctx.executeQueryAsync(onSuccess, this.requestErrorEventHandler);
+            }
+        });
+
+    }
+}
--- src/scripts/actions/spSiteContent/spSiteContent.tsx
+++ src/scripts/actions/spSiteContent/spSiteContent.tsx
@@ -4,6 +4,7 @@
 import * as React from 'react';
 import { WorkingOnIt } from './../common/WorkingOnIt';
 import MessageBar from './../common/MessageBar';
+import api from './../api/SpSiteContent'
 
 import { Checkbox } from 'office-ui-fabric-react/lib/Checkbox';
 import { MessageBarType } from 'office-ui-fabric-react/lib/MessageBar';
@@ -23,7 +24,7 @@ interface SpSiteContentState {
     message: string,
     showAll: boolean,
     openInNewTab: boolean,
-    filterText:string
+    filterText: string
 }
 
 export default class SpSiteContent extends React.Component<SpSiteContentProps, SpSiteContentState> {
@@ -39,54 +40,8 @@ export default class SpSiteContent extends React.Component<SpSiteContentProps, S
             openInNewTab: true,
             filterText: ''
         } as SpSiteContentState;
-        
+
         this.onFilterChange = this.onFilterChange.bind(this);
-    }
-    private getLists() {
-        let ctx = SP.ClientContext.get_current();
-        let web = ctx.get_web();
-
-        let siteConetent = web.get_lists();
-        ctx.load(web);
-        ctx.load(siteConetent, 'Include(RootFolder,Title,Id,Hidden,ItemCount,Created,ImageUrl,LastItemModifiedDate,Description,ParentWebUrl,DefaultNewFormUrl)');
-
-        const onSuccess =  (sender: any, err: any) => {
-
-            let items: Array<ISiteContent> = [], listEnumerator: any = siteConetent.getEnumerator();
-
-            while (listEnumerator.moveNext()) {
-                let oList: any = listEnumerator.get_current();
-                let listId: any = oList.get_id();
-                let listItem: ISiteContent = {
-                    id: listId,
-                    title: oList.get_title(),
-                    description: oList.get_description(),
-                    hidden: oList.get_hidden(),
-                    itemCount: oList.get_itemCount(),
-                    imageUrl: oList.get_imageUrl(),
-                    created: oList.get_created(),
-                    lastModified: oList.get_lastItemModifiedDate(),
-                    listUrl: oList.get_rootFolder().get_serverRelativeUrl(),
-                    settingsUrl: oList.get_parentWebUrl() + '/_layouts/15/listedit.aspx?List=' + listId,
-                    newFormUrl:oList.get_defaultNewFormUrl(),
-                    permissionsPageUrl: `${oList.get_parentWebUrl()}/_layouts/15/user.aspx?obj=%7B${listId}%7D,list&List=%7B${listId}%7D`,
-                }
-                items.push(listItem);
-            }
-            items.sort(function (a, b) {
-                return a.title.localeCompare(b.title);
-            });
-            this.setState({
-                siteLists: items,
-                isWorkingOnIt: false
-            } as SpSiteContentState);
-        };
-        let onError=  (sender: any, err: any) => {
-            SP.UI.Notify.addNotification("Failed to get web lists...<br>" + err.get_message(), false);
-            console.log(err);
-            this.props.closeWindowFunction();
-        };
-        ctx.executeQueryAsync(onSuccess, onError);
     }
     private showAll(e: any) {
         let showAllNewVal: boolean = e.target.checked;
@@ -113,9 +68,18 @@ export default class SpSiteContent extends React.Component<SpSiteContentProps, S
         } as SpSiteContentState);
     }
     private componentDidMount() {
-        this.getLists();
+        api.getLists().then((items: Array<ISiteContent>) => {
+            this.setState({
+                siteLists: items,
+                isWorkingOnIt: false
+            } as SpSiteContentState);
+        }).catch((reason: any) => {
+            SP.UI.Notify.addNotification("Failed to get web lists...<br>" + reason, false);
+            console.log(reason);
+            this.props.closeWindowFunction();
+        });
     }
-    
+
     private onFilterChange(str: string) {
         this.setState({ filterText: str } as SpSiteContentState);
     }
@@ -132,7 +96,7 @@ export default class SpSiteContent extends React.Component<SpSiteContentProps, S
                 });
             }
             const filter = this.state.filterText.toLowerCase();
-            if(filter !== ''){
+            if (filter !== '') {
                 lists = lists.filter((list: ISiteContent, index: number) => {
                     return list.title.toLowerCase().indexOf(filter) >= 0;
                 });
@@ -141,10 +105,10 @@ export default class SpSiteContent extends React.Component<SpSiteContentProps, S
             return (
                 <div className="action-container sp-siteContent">
                     <MessageBar message={this.state.message} messageType={this.state.messageType} showMessage={this.state.showMessage} />
-                    <div className="ms-Grid filters-container"> 
+                    <div className="ms-Grid filters-container">
                         <div className="ms-Grid-row">
                             <div className="ms-Grid-col ms-u-sm6 ms-u-md6 ms-u-lg6">
-                                <SearchBox onChange={this.onFilterChange}/>
+                                <SearchBox onChange={this.onFilterChange} />
                             </div>
                             <div className="ms-Grid-col ms-u-sm3 ms-u-md3 ms-u-lg3">
                                 <Checkbox label='Show all' defaultChecked={this.state.showAll} onChange={this.showAll.bind(this)} /></div>
@@ -153,30 +117,30 @@ export default class SpSiteContent extends React.Component<SpSiteContentProps, S
                             </div>
                         </div>
                     </div>
-                   
-                        <List
-                            items={lists}
-                            onRenderCell={(item, index) => (
-                                <div className='ms-ListBasicExample-itemCell' data-is-focusable={true}>
-                                    <Image className={'ms-ListBasicExample-itemImage' + (item.hidden ? ' hidden-spList' : '')} src={item.imageUrl} width={25} height={25} />
-                                    <div className='ms-ListBasicExample-itemContent'>
-                                        <a title={item.title} alt={item.title} href={item.listUrl} className='ms-ListBasicExample-itemName ms-font-l ms-fontColor-themePrimary ms-fontWeight-semibold' target={target}>
-                                            {item.title}
-                                        </a>
-                                        <div className='ms-ListBasicExample-itemIndex'>{`${item.itemCount} Items`}</div>
-                                        {
-                                            item.newFormUrl 
-                                                ?   <a target={target} href={item.newFormUrl} title="New Item" className="ms-ListItem-action"><i className="ms-Icon ms-Icon--AddTo"></i>New Item</a>
-                                                : null 
-                                        }
-                                    </div>
-                                    <div className="ms-ListItem-actions">
-                                        <a target={target} href={item.settingsUrl} title="Settings" className="ms-ListItem-action"><i className="ms-Icon ms-Icon--Settings"></i></a>
-                                        <a target={target} href={item.permissionsPageUrl} title={`Permissions: ${item.title}`} className="ms-ListItem-action"><i className="ms-Icon ms-Icon--SecurityGroup"></i></a>
-                                    </div>
+
+                    <List
+                        items={lists}
+                        onRenderCell={(item, index) => (
+                            <div className='ms-ListBasicExample-itemCell' data-is-focusable={true}>
+                                <Image className={'ms-ListBasicExample-itemImage' + (item.hidden ? ' hidden-spList' : '')} src={item.imageUrl} width={25} height={25} />
+                                <div className='ms-ListBasicExample-itemContent'>
+                                    <a title={item.title} alt={item.title} href={item.listUrl} className='ms-ListBasicExample-itemName ms-font-l ms-fontColor-themePrimary ms-fontWeight-semibold' target={target}>
+                                        {item.title}
+                                    </a>
+                                    <div className='ms-ListBasicExample-itemIndex'>{`${item.itemCount} Items`}</div>
+                                    {
+                                        item.newFormUrl
+                                            ? <a target={target} href={item.newFormUrl} title="New Item" className="ms-ListItem-action"><i className="ms-Icon ms-Icon--AddTo"></i>New Item</a>
+                                            : null
+                                    }
                                 </div>
-                            )}
-                            />
+                                <div className="ms-ListItem-actions">
+                                    <a target={target} href={item.settingsUrl} title="Settings" className="ms-ListItem-action"><i className="ms-Icon ms-Icon--Settings"></i></a>
+                                    <a target={target} href={item.permissionsPageUrl} title={`Permissions: ${item.title}`} className="ms-ListItem-action"><i className="ms-Icon ms-Icon--SecurityGroup"></i></a>
+                                </div>
+                            </div>
+                        )}
+                        />
                 </div>);
 
         }
