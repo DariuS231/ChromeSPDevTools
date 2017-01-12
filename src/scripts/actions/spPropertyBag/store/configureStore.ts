ffaf6a5cc04f42dd5aa704c086a01776d5ce845b
DariuS231 <dario17231@gmail.com>
2017-01-12 11:48:43 +0000

    Added support for redux dev tool

--- src/scripts/actions/spPropertyBag/store/configureStore.ts
+++ src/scripts/actions/spPropertyBag/store/configureStore.ts
@@ -1,8 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
+
+import { createStore, applyMiddleware, compose } from 'redux'
 import { rootReducer } from '../reducers/index'
 import * as inmmutable from 'redux-immutable-state-invariant'
 import thunk  from 'redux-thunk'; 
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 export const configureStore = (initialState:any) => {
-    return createStore(rootReducer, initialState, applyMiddleware(thunk, inmmutable()))
+    return createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(thunk, inmmutable())));
 }
