dd13f169fc32942ac5473b1997b147080d59da80
Dario Alvarez <dario17231@gmail.com>
2017-01-25 00:00:11 +0000

    Fixed update CA promise error

--- src/scripts/actions/spCustomActions/api/spCustomActionsApi.ts
+++ src/scripts/actions/spCustomActions/api/spCustomActionsApi.ts
@@ -48,7 +48,7 @@ export default class SpCustomActionsApi extends ApiBase {
 
             ca.deleteObject();
             ctx.executeQueryAsync((sender: any, err: any) => {
-                resolve(ca);
+                resolve(caObj);
             }, this.requestErrorEventHandler.bind(this));
         })
     }
@@ -92,7 +92,7 @@ export default class SpCustomActionsApi extends ApiBase {
 
             ca.update();
             ctx.executeQueryAsync((sender: any, err: any) => {
-                resolve(ca);
+                resolve(caObj);
             }, this.requestErrorEventHandler.bind(this));
         });
     }
