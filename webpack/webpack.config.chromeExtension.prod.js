d372bb31b84060b6a5322e321e684cbb358e0cf9
jquintozamora <quintorel@hotmail.com>
2017-02-01 15:24:25 +0000

    remove babel as it is not needed

--- webpack/webpack.config.chromeExtension.prod.js
+++ webpack/webpack.config.chromeExtension.prod.js
@@ -71,15 +71,28 @@ module.exports = {
     ],
     module: {
         loaders: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
             {
                 test: /\.ts(x?)$/,
                 exclude: /node_modules/,
-                loader: 'babel-loader?presets[]=es2016&presets[]=es2015&presets[]=react!ts-loader'
+                loader: 'awesome-typescript-loader'
             },
             {
                 test: /\.scss$/i,
                 loader: ExtractTextPlugin.extract("style", "css!sass")
             }
+        ],
+        preLoaders: [
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { test: /\.js$/, loader: "source-map-loader" }
         ]
-    }
+    },
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    // externals: {
+    //     "react": "React",
+    //     "react-dom": "ReactDOM"
+    // }
 };
