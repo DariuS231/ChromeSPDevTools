faa769ba97e2df6cd0696a5acc447370c126e81d
Dario Alvarez <dario17231@gmail.com>
2017-02-19 19:09:53 +0000

    Checking if supporter.

--- src/scripts/chromeExtension/background.ts
+++ src/scripts/chromeExtension/background.ts
@@ -3,28 +3,11 @@ chrome.browserAction.setBadgeBackgroundColor({ color: "red" });
 chrome.browserAction.setBadgeText({ text: "X" });
 chrome.browserAction.setTitle({ title: "DISABLED - Not a SharePoint site." });
 
-chrome.browserAction.onClicked.addListener((tab) => {
-    chrome.tabs.executeScript(tab.id, {
-        file: "scripts/spModalLauncher.js"
-    });
-});
-
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    if (changeInfo.status === "complete") {
-        chrome.tabs.executeScript(tabId, {
-            code: `(function() {
-                console.log(\"Reading!\");
-				var meta = document.querySelector(\"meta[name='GENERATOR']\");
-                var valueToRet = ((meta && meta.content === "Microsoft SharePoint") || (window.location.host.endsWith(".sharepoint.com")));
-                console.log(\"Is SP: \" + valueToRet );
-				return valueToRet;
-			})()`
-        }, (result) => {
-            if (result && result.length > 0 && result[0]) {
-                chrome.browserAction.enable(tabId);
-                chrome.browserAction.setBadgeText({ text: "", tabId });
-                chrome.browserAction.setTitle({ title: "Click to show the Properties modal.", tabId });
-            }
-        });
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    const tabId = sender.tab.id;
+    if (request.isSp) {
+        chrome.browserAction.enable(tabId);
+        chrome.browserAction.setBadgeText({ text: "", tabId });
+        chrome.browserAction.setTitle({ title: "Click to show the Properties modal.", tabId });
     }
 });
