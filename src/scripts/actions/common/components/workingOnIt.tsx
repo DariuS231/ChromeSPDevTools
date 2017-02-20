794b12ee074be86b82b527c69ddaad592dcfe61a
DariuS231 <dario17231@gmail.com>
2017-02-20 01:24:07 +0000

    Changed working on it design.

--- src/scripts/actions/common/components/workingOnIt.tsx
+++ src/scripts/actions/common/components/workingOnIt.tsx
@@ -1,7 +1,10 @@
 
+import {
+  Spinner,
+  SpinnerType
+} from "office-ui-fabric-react/lib/Spinner";
 import * as React from "react";
 
 export const  WorkingOnIt = () => <div className="working-on-it-wrapper">
-    <img src="/_layouts/images/gears_anv4.gif" alt="Working on it..." />
-    <h2>Working on it...</h2>
+    <Spinner type={ SpinnerType.large } label="Working on it..." />
 </div>;
