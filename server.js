88f18c9ff1d88d1f9de9625eb745c2d166a839d4
Dario Alvarez <dario17231@gmail.com>
2017-10-09 00:56:01 +0100

    Fixed build issues

--- server.js
+++ server.js
@@ -13,32 +13,30 @@ var ops = {
 };
 
 new WebpackDevServer(webpack(config), {
-    // All the options for webpack-dev-server here: https://webpack.js.org/configuration/devtool/
-    contentBase: config.output.path,
-    publicPath: config.output.publicPath,
-    // All the stats options here: https://webpack.js.org/configuration/stats/
-    stats: {
-        colors: true, // color is life
-        chunks: false, // this reduces the amount of stuff I see in my terminal; configure to your needs
-        'errors-only': true
-    },
-    https: true,
-    headers: {
-      'Access-Control-Allow-Origin': '*'
-    },
-    cert: fs.readFileSync(ops.cert),
-    ca: fs.readFileSync(ops.cert),
-    key: fs.readFileSync(ops.key),
-    //requestCert: true,
-    //rejectUnauthorized: true,
-    hot: false,
-    inline: false,
-    historyApiFallback: true
-})
-.listen(port, host, (err) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(`Listening at ${host}:${port}`);
-    //opener(`https://${host}:${port}`);
-});
+        // All the options for webpack-dev-server here: https://webpack.js.org/configuration/devtool/
+        contentBase: config.output.path,
+        publicPath: config.output.publicPath,
+        // All the stats options here: https://webpack.js.org/configuration/stats/
+        stats: {
+            colors: true, // color is life
+            chunks: false, // this reduces the amount of stuff I see in my terminal; configure to your needs
+            'errors-only': true
+        },
+        https: true,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        https: true,
+        //requestCert: true,
+        //rejectUnauthorized: true,
+        hot: false,
+        inline: false,
+        historyApiFallback: true
+    })
+    .listen(port, host, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log(`Listening at ${host}:${port}`);
+        //opener(`https://${host}:${port}`);
+    });
