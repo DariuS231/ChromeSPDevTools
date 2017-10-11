f818d59c3109328499e19e4a9709e7275f003eb3
Dario Alvarez <Dario.Alvarez@contentandcode.com>
2017-10-11 17:49:59 +0100

    Initial Api Request

--- src/scripts/actions/spSearch/api/spSearchApi.ts
+++ src/scripts/actions/spSearch/api/spSearchApi.ts
@@ -1,6 +1,20 @@
 import ApiBase from "./../../common/apiBase";
 import { constants } from "./../constants/constants";
+import { IInitialState, ISearchResult } from "../interfaces/spSearchInterfaces";
 
 export default class SpSearchApi extends ApiBase {
+    public getResults(state: IInitialState): Promise<ISearchResult[]> {
+        return new Promise((resolve, reject) => {
+            this.getWebUrl().then(webUrl => {
+                const reqUrl = `${webUrl}/_api/search/query?querytext='*'&trimduplicates=false&selectproperties='Title%2cId%2cRefinableString43%2cPath'&refinementfilters='RefinableString43:test'&clienttype='ContentSearchRegular'`;
+                this.getRequest(reqUrl).then((response: any) => {
+                    const results: ISearchResult[] = [];
 
+                    resolve(results);
+                }).catch((error: any) => {
+                    reject(error);
+                });
+            });
+        });
+    }
 }
